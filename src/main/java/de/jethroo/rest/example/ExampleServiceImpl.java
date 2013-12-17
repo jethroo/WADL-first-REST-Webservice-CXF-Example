8435cef1a175067688c7a6b608ac583aea403e1a
Carsten Wirth <carsten.wirth@blacklane.com>
2013-12-17 13:51:04 +0100

    added config for logback and first service implementation class

---
+++ src/main/java/de/jethroo/rest/example/ExampleServiceImpl.java
@@ -0,0 +1,25 @@
+package de.jethroo.rest.example;
+
+public class ExampleServiceImpl implements Thingies {
+
+	public void onRetrieve(String id) {
+		// TODO Auto-generated method stub
+
+	}
+
+	public void onList(String id) {
+		// TODO Auto-generated method stub
+
+	}
+
+	public void onUpdate(String id, String attribute_name) {
+		// TODO Auto-generated method stub
+
+	}
+
+	public void onDelete(String id) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
