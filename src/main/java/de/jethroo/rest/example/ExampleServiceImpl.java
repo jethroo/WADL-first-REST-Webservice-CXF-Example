64d1383c256f84c0ad7aac77c33cedc96d18af1a
Carsten Wirth <carsten.wirth@blacklane.com>
2013-12-17 21:34:10 +0100

    adepted SOAP-UI wadl manually for resourcefull routing/paths

--- src/main/java/de/jethroo/rest/example/ExampleServiceImpl.java
+++ src/main/java/de/jethroo/rest/example/ExampleServiceImpl.java
@@ -1,5 +1,8 @@
 package de.jethroo.rest.example;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
 
@@ -8,40 +11,43 @@ import org.slf4j.LoggerFactory;
 
 import com.google.gson.Gson;
 
-
 public class ExampleServiceImpl implements Thingies {
 
 	private final Gson serializer = new Gson();
 
 	private final Logger logger = LoggerFactory.getLogger(ExampleServiceImpl.class);
 
-	public Response onRetrieve(String id) {
-		logger.debug("recieved GET request for thingies with id : "+id);
-		try {
-			int resource_id = Integer.parseInt(id);
-			// just generate a dummy resource for this request 
-			Thingy somethingy = new Thingy(resource_id, "attribute_value_for_id"+resource_id);
+	public Response onRetrieve(int id) {
+		logger.debug("recieved GET request for thingies with id : " + id);
 
-			return Response.ok(serializer.toJson(somethingy), MediaType.APPLICATION_JSON).build();
-		} catch (java.lang.NumberFormatException e) {
-			return Response.status(400).build();
+		// just generate a dummy resource for this request
+		Thingy somethingy = new Thingy(id, "attribute_value_for_id_" + id);
+		return Response.ok(serializer.toJson(somethingy), MediaType.APPLICATION_JSON).build();
+	}
+
+	public Response onList() {
+		logger.debug("recieved GET request for thingies (index)");
+
+		// just generate a dummy resource for this request
+		List<Thingy> thingies = new ArrayList<Thingy>();
+		for(int i=1; i < 5; i++){
+			thingies.add(new Thingy(i, "foo_"+i));
 		}
-		
+		return Response.ok(serializer.toJson(thingies), MediaType.APPLICATION_JSON).build();
 	}
 
-	public void onList(String id) {
+	public Response onUpdate(int id, String attribute_name) {
 		// TODO Auto-generated method stub
-
+		return null;
 	}
 
-	public void onUpdate(String id, String attribute_name) {
+	public Response onDelete(int id) {
 		// TODO Auto-generated method stub
-
+		return null;
 	}
 
-	public void onDelete(String id) {
-		// TODO Auto-generated method stub
 
-	}
+
+	
 
 }
