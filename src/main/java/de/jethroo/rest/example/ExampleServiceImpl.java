e377c8f08ec91cb4db1f7d7648f9fd51dacf8e94
Carsten Wirth <carsten.wirth@blacklane.com>
2013-12-18 23:58:30 +0100

    wee first test up and running

--- src/main/java/de/jethroo/rest/example/ExampleServiceImpl.java
+++ src/main/java/de/jethroo/rest/example/ExampleServiceImpl.java
@@ -17,13 +17,24 @@ public class ExampleServiceImpl implements Thingies {
 	private final Gson serializer = new Gson();
 
 	private final Logger logger = LoggerFactory.getLogger(ExampleServiceImpl.class);
+	
+	@Autowired
+	private ThingyDao dao;
+	
+	public void setDao(ThingyDao dao) {
+		this.dao = dao;
+	}
 
 	public Response onRetrieve(int id) {
 		logger.debug("recieved GET request for thingies with id : " + id);
-
-		// just generate a dummy resource for this request
-		Thingy somethingy = new Thingy(id, "attribute_value_for_id_" + id);
-		return Response.ok(serializer.toJson(somethingy), MediaType.APPLICATION_JSON).build();
+		Thingy result;
+		if (id > 0) {
+			result = dao.findById(id);
+			if (result != null) {
+				return Response.ok(serializer.toJson(result), MediaType.APPLICATION_JSON).build();
+			}
+		}
+		return Response.noContent().build();
 	}
 
 	public Response onList() {
@@ -31,8 +42,8 @@ public class ExampleServiceImpl implements Thingies {
 
 		// just generate a dummy resource for this request
 		List<Thingy> thingies = new ArrayList<Thingy>();
-		for(int i=1; i < 5; i++){
-			thingies.add(new Thingy(i, "foo_"+i));
+		for (int i = 1; i < 5; i++) {
+			thingies.add(new Thingy(i, "foo_" + i));
 		}
 		return Response.ok(serializer.toJson(thingies), MediaType.APPLICATION_JSON).build();
 	}
@@ -40,7 +51,7 @@ public class ExampleServiceImpl implements Thingies {
 	public Response onUpdate(int id, String attribute_name) {
 		ThingyDao dao = new ThingyDao();
 		Thingy thing = dao.findById(id);
-		if (thing != null){
+		if (thing != null) {
 			thing.setAttribute_name(attribute_name);
 		} else {
 			thing = new Thingy(attribute_name);
@@ -53,9 +64,5 @@ public class ExampleServiceImpl implements Thingies {
 		// TODO Auto-generated method stub
 		return null;
 	}
-
-
-
-	
 
 }
--- src/test/java/de/jethroo/rest/example/ExampleServiceTest.java
+++ src/test/java/de/jethroo/rest/example/ExampleServiceTest.java
@@ -1,15 +1,16 @@
 package de.jethroo.rest.example;
 
-import static org.junit.Assert.fail;
-
 import javax.ws.rs.core.Response;
 
+import org.junit.Assert;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import org.springframework.transaction.annotation.Transactional;
+
+import com.google.gson.Gson;
 
 @RunWith(SpringJUnit4ClassRunner.class)
 @ContextConfiguration("file:src/test/resources/test-service-context.xml")
@@ -19,13 +20,19 @@ public class ExampleServiceTest {
 	@Autowired
 	private ThingyDao dao;
 	
+	@Autowired
+    private Thingies service;
+	
 	@Test
 	public void testOnRetrieve() {
-		dao.findById(1);
-		dao.insert(new Thingy("something"));
-		Thingies service = new ExampleServiceImpl();
-		Response response = service.onRetrieve(1);
-		//fail("Not yet implemented");
+		Thingy thingy = new Thingy("something");
+		dao.insert(thingy);
+		Response response = service.onRetrieve(thingy.getId());
+		Gson gson = new Gson();
+		Thingy response_thingy = gson.fromJson(response.getEntity().toString(), Thingy.class);
+		Assert.assertEquals(thingy.getId(), response_thingy.getId());
+		Assert.assertEquals(thingy.getAttribute_name(), response_thingy.getAttribute_name());
+		Assert.assertEquals(200,response.getStatus());
 	}
 
 	/*
